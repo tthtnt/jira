e4699f3bd44c6d5fee1a9f56d0c865fa9d015bf4
Jack.liu <jack.liu@ihr360.com>
2023-12-04 16:44:49 +0800

    feat: 添加工程列表

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { ProjectListScreen } from "screens/project-list";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>test</p>
-        <p>test2</p>
-        <p>test3</p>
-        <p>test4</p>
-      </header>
+      <ProjectListScreen />
     </div>
   );
 }
