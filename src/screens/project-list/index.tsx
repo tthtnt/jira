348c01cef96ba029aacfc620c004d31839693f34
Jack.liu <jack.liu@ihr360.com>
2023-12-25 17:50:11 +0800

    feat: 用useDocumentTitle 改变文档标题

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,12 +1,10 @@
-import { cleanObject, useDebounce, useMount } from "utils";
+import { cleanObject, useDebounce, useDocumentTitle, useMount } from "utils";
 import { List, Project } from "./list";
 import { SearchPanel } from "./search-pannel";
-import { useEffect, useState } from "react";
-import { useHttp } from "utils/http";
+import { useState } from "react";
 import React from "react";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
-import { useAsync } from "utils/use-async";
 import { useProjects } from "utils/project";
 import { useUsers } from "utils/user";
 
@@ -18,6 +16,8 @@ export const ProjectListScreen = () => {
   const debouncedParam = useDebounce(param, 500);
   const { isLoading, error, data: list } = useProjects(debouncedParam);
   const { data: users } = useUsers();
+
+  useDocumentTitle("项目列表", false);
 
   return (
     <Container>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -74,3 +74,21 @@ export const useArray = <P>(personsValue: P[]) => {
     clear,
   };
 };
+
+export const useDocumentTitle = (
+  title: string,
+  keepOnUnmount: boolean = true
+) => {
+  const oldTitle = document.title;
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle;
+      }
+    };
+  }, []);
+};
