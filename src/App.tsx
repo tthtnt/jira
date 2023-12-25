12158ff37e845e374b94da139c963d8762c32bc6
Jack.liu <jack.liu@ihr360.com>
2023-12-25 17:05:30 +0800

    feat: 添加error-boundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { useAuth } from "context/auth-context";
 import "./App.css";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticateddApp } from "unauthenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
 
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticateddApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticateddApp />}
+      </ErrorBoundary>
     </div>
   );
 }
