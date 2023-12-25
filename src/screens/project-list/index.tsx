15c3011fb2c765d197c5e56b650072b103f0df00
Jack.liu <jack.liu@ihr360.com>
2023-12-25 14:54:28 +0800

    feat: 用高级hook useAsync统一处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,51 +1,32 @@
 import { cleanObject, useDebounce, useMount } from "utils";
-import { List } from "./list";
+import { List, Project } from "./list";
 import { SearchPanel } from "./search-pannel";
 import { useEffect, useState } from "react";
 import { useHttp } from "utils/http";
 import React from "react";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
+import { useAsync } from "utils/use-async";
+import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoding] = useState(false);
-  const [error, setError] = useState<null | Error>(null);
-
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
   const debouncedParam = useDebounce(param, 500);
-
-  const [list, setList] = useState([]);
-
-  const client = useHttp();
-
-  useEffect(() => {
-    setIsLoding(true);
-    client("projects", { data: cleanObject(debouncedParam) })
-      .then(setList)
-      .catch((error) => {
-        setError(error);
-        setList([]);
-      })
-      .finally(() => setIsLoding(false));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam]);
-
-  useMount(() => {
-    client("users").then(setUsers);
-  });
+  const { isLoading, error, data: list } = useProjects(debouncedParam);
+  const { data: users } = useUsers();
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={"danger"}>{error.message}</Typography.Text>
       ) : null}
-      <List loading={isLoading} users={users} dataSource={list} />
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
     </Container>
   );
 };
---
+++ src/utils/project.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useAsync } from "./use-async";
+import { cleanObject } from "utils";
+import { Project } from "screens/project-list/list";
+import { useHttp } from "./http";
+
+export const useProjects = (param?: Partial<Project>) => {
+  const client = useHttp();
+  const { run, ...result } = useAsync<Project[]>();
+
+  useEffect(() => {
+    run(client("projects", { data: cleanObject(param || {}) }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [param]);
+
+  return result;
+};
